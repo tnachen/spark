11418f4cdbf7fb5ed30e503d80b59a553a49402d
Iulian Dragos <jaguarul@gmail.com>
2015-05-21 16:46:02 +0200

    [WIP] Add persistent shuffle directory

--- core/src/main/scala/org/apache/spark/scheduler/cluster/mesos/CoarseMesosSchedulerBackend.scala
+++ core/src/main/scala/org/apache/spark/scheduler/cluster/mesos/CoarseMesosSchedulerBackend.scala
@@ -328,7 +328,8 @@ private[spark] class CoarseMesosSchedulerBackend(
   override def doRequestTotalExecutors(requestedTotal: Int): Boolean = {
     // We don't truly know if we can fulfill the full amount of executors
     // since at coarse grain it depends on the amount of slaves available.
-    logInfo("Capping the total amount of executors to " + requestedTotal)
+    if (executorLimitOption.map(_ != requestedTotal).getOrElse(true))
+      logInfo("Capping the total amount of executors to " + requestedTotal)
     executorLimitOption = Option(requestedTotal)
     true
   }
